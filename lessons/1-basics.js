e3c83de23583864bbe8d7fae235d134f7f51406a
Colin Chang <colinichang@gmail.com>
2019-05-19 11:08:31 -0400

    Explicitly list lessons in lesson

--- lessons/1-basics.js
+++ lessons/1-basics.js
@@ -1,6 +1,16 @@
 /**
  * Please convert this file to Typescript.
  * You might discover a bug!
+ *
+ * Lessons:
+ *  Typescript language features:
+ *   1. Parameter types
+ *   2. Return types
+ *   3. Interfaces
+ *
+ *  VSCode features
+ *   1. Autocomplete
+ *   2. Hovering
  */
 
 function sortByName(people) {
